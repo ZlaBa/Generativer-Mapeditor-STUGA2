2cd938f27eca70cf06dfa973f77f1ed52c278372
Mario <mario.bernasconi@stud.hslu.ch>
2020-03-20 17:35:01 +0100

    Ansicht der Spielwetl gefixt. WaldScript optimiert.

---
+++ Assets/Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform CameraMovement;
+    public float dampTime = 0.4f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        cameraPos = new Vector3(CameraMovement.position.x, CameraMovement.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+}
